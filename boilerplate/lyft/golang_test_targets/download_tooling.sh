ff22a2a711aa79b088f92a6c60e91d9478188ac1
Hongxin Liang <honnix@spotify.com>
2020-01-08 12:50:09 +0100

    address comments

--- boilerplate/lyft/golang_test_targets/download_tooling.sh
+++ boilerplate/lyft/golang_test_targets/download_tooling.sh
@@ -4,7 +4,7 @@
 # The reason we cannot turn off module entirely and install is that we need the replace statement in go.mod
 # because we are installing a mockery fork. Turning it off would result installing the original not the fork.
 # However, because the installation of these tools themselves sometimes modifies the go.mod/go.sum files. We don't
-# want this either. So instead, we're going to copy those files into a temporary directory, do the installation, and=
+# want this either. So instead, we're going to copy those files into a temporary directory, do the installation, and
 # ignore any changes made to the go mod files.
 # (See https://github.com/golang/go/issues/30515 for some background context)
 
@@ -12,21 +12,25 @@ set -e
 
 go_install_tool () {
   tmp_dir=$(mktemp -d -t gotooling-XXXXXXXXXX)
-  echo "Installing $1 inside ${tmp_dir}"
+  echo "Installing $1 inside $tmp_dir"
   cp go.mod go.sum "$tmp_dir"
   pushd "$tmp_dir"
-  go get $1
+  go get "$1"
   popd
 }
 
+# List of tools to go get
+# In the format of "<cli>:<package>" or ":<package>" if no cli
 tools=(
-  "github.com/vektra/mockery/cmd/mockery"
-  "github.com/lyft/flytestdlib/cli/pflags"
-  "github.com/golangci/golangci-lint/cmd/golangci-lint"
-  "github.com/alvaroloes/enumer"
+  "mockery:github.com/vektra/mockery/cmd/mockery"
+  "pflags:github.com/lyft/flytestdlib/cli/pflags"
+  "golangci-lint:github.com/golangci/golangci-lint/cmd/golangci-lint"
+  "enumer:github.com/alvaroloes/enumer"
 )
 
 for tool in "${tools[@]}"
 do
-    go_install_tool $tool
+    cli=$(echo "$tool" | cut -d':' -f1)
+    package=$(echo "$tool" | cut -d':' -f2)
+    command -v "$cli" || go_install_tool "$package"
 done
