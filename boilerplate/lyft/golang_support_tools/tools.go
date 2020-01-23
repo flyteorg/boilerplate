19ae2a9597d112df3a2f9ff4667b104ed097b8d0
Yee Hing Tong <ytong@lyft.com>
2020-01-23 11:22:02 -0800

    forgot about enumer

--- boilerplate/lyft/golang_support_tools/tools.go
+++ boilerplate/lyft/golang_support_tools/tools.go
@@ -6,4 +6,5 @@ import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/lyft/flytestdlib/cli/pflags"
 	_ "github.com/vektra/mockery/cmd/mockery"
+	_ "github.com/alvaroloes/enumer"
 )
