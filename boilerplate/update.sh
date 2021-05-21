5b4f60c033578dc0254521ec41b647f27c86496d
Yuvraj <evalsocket@gmail.com>
2021-05-21 21:19:47 +0530

    Update update.sh script after everything is done
    Signed-off-by: Yuvraj <evalsocket@gmail.com>

--- boilerplate/update.sh
+++ boilerplate/update.sh
@@ -15,61 +15,64 @@ trap 'rm -fr $OUT' EXIT
 git clone https://github.com/flyteorg/boilerplate.git "${OUT}"
 
 echo "Updating the update.sh script."
-cp "${OUT}/boilerplate/update.sh" "${DIR}/update.sh"
-echo ""
+if [ $(cat ${OUT}/boilerplate/update.sh) != $(cat ${DIR}/update.sh) ];then 
+    echo ""
+    cd "${DIR}"
+    sh "${OUT}/boilerplate/update.sh"
+    cp "${OUT}/boilerplate/update.sh" "${DIR}/update.sh"
+else  
+  CONFIG_FILE="${DIR}/update.cfg"
+  README="https://github.com/flyteorg/boilerplate/blob/master/Readme.rst"
 
-
-CONFIG_FILE="${DIR}/update.cfg"
-README="https://github.com/flyteorg/boilerplate/blob/master/Readme.rst"
-
-if [ ! -f "$CONFIG_FILE" ]; then
-  echo "$CONFIG_FILE not found."
-  echo "This file is required in order to select which features to include." 
-  echo "See $README for more details."
-  exit 1
-fi
-
-if [ -z "$REPOSITORY" ]; then
-  echo "$REPOSITORY is required to run this script"
-  echo "See $README for more details."
-  exit 1
-fi
-
-while read -r directory junk; do
-  # Skip comment lines (which can have leading whitespace)
-  if [[ "$directory" == '#'* ]]; then
-    continue
-  fi
-  # Skip blank or whitespace-only lines
-  if [[ "$directory" == "" ]]; then
-    continue
-  fi
-  # Lines like
-  #    valid/path  other_junk
-  # are not acceptable, unless `other_junk` is a comment
-  if [[ "$junk" != "" ]] && [[ "$junk" != '#'* ]]; then
-    echo "Invalid config! Only one directory is allowed per line. Found '$junk'"
+  if [ ! -f "$CONFIG_FILE" ]; then
+    echo "$CONFIG_FILE not found."
+    echo "This file is required in order to select which features to include." 
+    echo "See $README for more details."
     exit 1
   fi
 
-  dir_path="${OUT}/boilerplate/${directory}"
-  # Make sure the directory exists
-  if ! [[ -d "$dir_path" ]]; then
-    echo "Invalid boilerplate directory: '$directory'"
+  if [ -z "$REPOSITORY" ]; then
+    echo "$REPOSITORY is required to run this script"
+    echo "See $README for more details."
     exit 1
   fi
 
-  echo "***********************************************************************************"
-  echo "$directory is configured in update.cfg."
-  echo "-----------------------------------------------------------------------------------"
-  echo "syncing files from source."
-  rm -rf "${DIR:?}/${directory}"
-  mkdir -p "$(dirname "${DIR}"/"${directory}")"
-  cp -r "$dir_path" "${DIR}/${directory}"
-  if [ -f "${DIR}/${directory}/update.sh" ]; then
-    echo "executing ${DIR}/${directory}/update.sh"
-    "${DIR}/${directory}/update.sh"
-  fi
-  echo "***********************************************************************************"
-  echo ""
-done < "$CONFIG_FILE"
+  while read -r directory junk; do
+    # Skip comment lines (which can have leading whitespace)
+    if [[ "$directory" == '#'* ]]; then
+      continue
+    fi
+    # Skip blank or whitespace-only lines
+    if [[ "$directory" == "" ]]; then
+      continue
+    fi
+    # Lines like
+    #    valid/path  other_junk
+    # are not acceptable, unless `other_junk` is a comment
+    if [[ "$junk" != "" ]] && [[ "$junk" != '#'* ]]; then
+      echo "Invalid config! Only one directory is allowed per line. Found '$junk'"
+      exit 1
+    fi
+
+    dir_path="${OUT}/boilerplate/${directory}"
+    # Make sure the directory exists
+    if ! [[ -d "$dir_path" ]]; then
+      echo "Invalid boilerplate directory: '$directory'"
+      exit 1
+    fi
+
+    echo "***********************************************************************************"
+    echo "$directory is configured in update.cfg."
+    echo "-----------------------------------------------------------------------------------"
+    echo "syncing files from source."
+    rm -rf "${DIR:?}/${directory}"
+    mkdir -p "$(dirname "${DIR}"/"${directory}")"
+    cp -r "$dir_path" "${DIR}/${directory}"
+    if [ -f "${DIR}/${directory}/update.sh" ]; then
+      echo "executing ${DIR}/${directory}/update.sh"
+      "${DIR}/${directory}/update.sh"
+    fi
+    echo "***********************************************************************************"
+    echo ""
+  done < "$CONFIG_FILE"
+fi 
